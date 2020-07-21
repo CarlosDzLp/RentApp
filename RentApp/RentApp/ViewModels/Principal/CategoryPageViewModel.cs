3c8d9aab9b58a749336d6e5fd8532bb465b31962
Carlos Diaz <ryankar90@hotmail.com>
2020-07-20 19:44:36 -0500

    add

---
+++ RentApp/RentApp/ViewModels/Principal/CategoryPageViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using RentApp.ViewModels.Base;
+
+namespace RentApp.ViewModels.Principal
+{
+    public class CategoryPageViewModel : BindableBase
+    {
+        #region Constructor
+        public CategoryPageViewModel(Guid idCategory)
+        {
+        }
+        #endregion
+    }
+}
